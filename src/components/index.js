41148741f2ecf4811fe9b47e688d1d778cda1574
Rany Virakvuth <ranyvirakvuth@gmail.com>
2024-06-16 17:45:13 +0700

    First commit to Bank Website, Completed Navbar section, and Hero section on progress

---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import Navbar from "./Navbar";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+import Billing from "./Billing";
+import Business from "./Business";
+import Button from "./Button";
+import CardDeal from "./CardDeal";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Feedback from "./Feedback";
+import Footer from "./Footer";
+import GetStarted from "./GetStarted";
+import Hero from "./Hero";
+
+export {
+    Navbar,
+    Stats,
+    Testimonials,
+    Billing,
+    Business,
+    Button,
+    CardDeal,
+    Clients,
+    CTA,
+    Feedback,
+    Footer,
+    GetStarted,
+    Hero
+}
